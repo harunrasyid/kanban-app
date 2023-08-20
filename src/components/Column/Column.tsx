5047187f1ab2a61936bdf72d9fc951af64774da3
harunrasyid <78939703+harunrasyid@users.noreply.github.com>
2023-08-20 10:06:42 +0700

    - Feat: add card UI and its styling
    Signed-off-by: harunar21 <harun@rekosistem.com>

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,8 +1,14 @@
 import styles from "./style.module.css";
+import { Card } from "../Card/Card.tsx";
 interface Props {
   title: string;
 }
 
 export const Column = ({ title }: Props) => {
-  return <div className={styles.column}>{title}</div>;
+  return (
+    <div className={styles.column}>
+      <p>{title}</p>
+      <Card title={"Task"} />
+    </div>
+  );
 };
