3f0f1df199785e28f5566df6d57dd14f256107d3
harunar21 <harun@rekosistem.com>
2023-08-22 21:59:52 +0700

    - Feat: add delete icon to the task card - Feat: add delete function in the store
    Signed-off-by: harunar21 <harun@rekosistem.com>

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,12 +4,17 @@ import { TaskType } from "../types/Types.ts";
 export interface StateType {
   tasks: TaskType[];
   addTasks(data: TaskType): void;
+  deleteTask(data: TaskType): void;
 }
 
 const store = (set): StateType => ({
   tasks: [],
   addTasks: (data: TaskType) =>
     set((state: StateType) => ({ tasks: [...state.tasks, data] })),
+  deleteTask: (data: TaskType) =>
+    set((state: StateType) => ({
+      tasks: state.tasks.filter((task) => task.title !== data.title),
+    })),
 });
 
 export const useStore = create(store);
